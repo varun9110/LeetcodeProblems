12f21d396b155bba724bc501e309cd0a628c1dda
varun9110 <varun.goel.vg@gmail.com>
2023-01-06 20:12:20 +0530

    feat: 1408 solution

---
+++ 1408_String_Matching_in_an_Array.js
@@ -0,0 +1,38 @@
+/*
+
+1408_String_Matching_in_an_Array
+Difficulty : Easy
+
+
+Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
+
+A substring is a contiguous sequence of characters within a string
+
+Example 1:
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+Explanation: No string of words is substring of another string.
+
+*/
+
+
+
+var stringMatching = function(words) {
+    let res = [];
+    for (let word of words){
+        if (words.filter(w => w.includes(word)).length > 1) res.push(word);
+    }
+    return res;
+};
