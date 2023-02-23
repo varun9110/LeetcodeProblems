ff1887520436b33573bfb04fe947bc93cc658816
varun9110 <varun.goel.vg@gmail.com>
2023-02-23 22:19:56 +0530

    faet: problem 121

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -0,0 +1,46 @@
+/*
+121. Best Time to Buy and Sell Stock
+Difficulty : easy
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+Constraints:
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+/*
+Approach 1 : 
+Brute force is the create a nested loop and find the max difference
+*/
+
+/*
+Approach 2 : 
+let min be the 0th index
+ and start the iteration from 1st index 
+then keep checking for min  from min vs ith index
+and also calculte the max between result  and ith index - min
+*/
+
+var maxProfit = function(prices) {
+    let result = 0;
+    let min = prices[0];
+    for(let i = 1; i < prices.length; i++) {
+        min = Math.min(prices[i], min);
+        result = Math.max(result, prices[i] - min);
+    }
+    return result;
+};
