c19581d3febd10eb5110f8438b8344473c5ab585
varun9110 <varun.goel.vg@gmail.com>
2023-02-27 20:48:05 +0530

    faet: Problem 1342

---
+++ 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.js
@@ -0,0 +1,63 @@
+/*
+1342. Number of Steps to Reduce a Number to Zero
+Difficulty: Easy
+
+Given an integer num, return the number of steps to reduce it to zero.
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+Example 1:
+Input: num = 14
+Output: 6
+Explanation: 
+Step 1) 14 is even; divide by 2 and obtain 7. 
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3. 
+Step 4) 3 is odd; subtract 1 and obtain 2. 
+Step 5) 2 is even; divide by 2 and obtain 1. 
+Step 6) 1 is odd; subtract 1 and obtain 0.
+
+Example 2:
+Input: num = 8
+Output: 4
+Explanation: 
+Step 1) 8 is even; divide by 2 and obtain 4. 
+Step 2) 4 is even; divide by 2 and obtain 2. 
+Step 3) 2 is even; divide by 2 and obtain 1. 
+Step 4) 1 is odd; subtract 1 and obtain 0.
+
+Example 3:
+Input: num = 123
+Output: 12
+
+Constraints:
+0 <= num <= 106
+*/
+
+/*
+Approach:
+As the problem suggests, if the while the number is greater than 0, and in that if the number is odd then reduce it by 1 else divide it by 2
+increment the result by 1 every time the while loop is executed. */
+
+var numberOfSteps = function(num) {
+    let result = 0;
+    while(num > 0){
+        if(num%2 === 0 ){
+            num = Math.floor(num/2);
+        } else {
+            num = num-1;
+        }
+        result++;
+    }
+    return result;
+};
+
+
+/* same logic but just simplified code */
+
+var numberOfSteps = function(num) {
+    var count = 0;
+    while (num !== 0) {
+      num % 2 === 0 ? ((num = num / 2), count++) : (num--, count++);
+    }
+    return count;
+  };
