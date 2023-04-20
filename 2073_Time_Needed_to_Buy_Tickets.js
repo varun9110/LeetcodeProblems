f83a773fdec59d00505111805269d6aa0d6af7c9
varun9110 <varun.goel.vg@gmail.com>
2023-04-20 22:50:24 +0530

    feat: Problem 2073

---
+++ 2073_Time_Needed_to_Buy_Tickets.js
@@ -0,0 +1,69 @@
+/**
+ * 2073. Time Needed to Buy Tickets
+ * Difficulty : Easy
+ * There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
+You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
+Each person takes exactly 1 second to buy a ticket. A person can only buy 1 ticket at a time and has to go back to the end of the line (which happens 
+instantaneously) in order to buy more tickets. If a person does not have any tickets left to buy, the person will leave the line.
+Return the time taken for the person at position k (0-indexed) to finish buying tickets.
+Example 1:
+Input: tickets = [2,3,2], k = 2
+Output: 6
+Explanation: 
+- In the first pass, everyone in the line buys a ticket and the line becomes [1, 2, 1].
+- In the second pass, everyone in the line buys a ticket and the line becomes [0, 1, 0].
+The person at position 2 has successfully bought 2 tickets and it took 3 + 3 = 6 seconds.
+Example 2:
+Input: tickets = [5,1,1,1], k = 0
+Output: 8
+Explanation:
+- In the first pass, everyone in the line buys a ticket and the line becomes [4, 0, 0, 0].
+- In the next 4 passes, only the person in position 0 is buying tickets.
+The person at position 0 has successfully bought 5 tickets and it took 4 + 1 + 1 + 1 + 1 = 8 seconds.
+Constraints:
+n == tickets.length
+1 <= n <= 100
+1 <= tickets[i] <= 100
+0 <= k < n
+*/
+
+/**
+ * Approach:
+ * Run a loop till k index, and check if the value is lesser than kth index, if it is then add the value to the result or else add the value of kth index to the
+ * result.
+ * then run another loop from k+1 til the end of the array. same condition but this time if the value is lesser then add the value or else add value-1
+ */
+
+var timeRequiredToBuy = function (tickets, k) {
+  let result = 0;
+  let value = tickets[k];
+  for (let i = 0; i <= k; i++) {
+    result += tickets[i] <= value ? tickets[i] : value;
+  }
+  for (let i = k + 1; i < tickets.length; i++) {
+    result += tickets[i] < value ? tickets[i] : value - 1;
+  }
+  return result;
+};
+
+
+/**
+ * Approach : 
+ * Iterate as the question says. tickets[k] number of times
+ * 
+ */
+var timeRequiredToBuy = function (tickets, k) {
+  let ans = 0;
+  let i = 0;
+  while (tickets[k] !== 0) {
+    if (i === tickets.length) {
+      i = 0;
+    }
+    if (tickets[i] != 0) {
+      tickets[i] -= 1;
+      ans++;
+    }
+    i++;
+  }
+  return ans;
+};
