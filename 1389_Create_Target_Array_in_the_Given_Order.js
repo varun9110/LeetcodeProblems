65827c632fa95b36c2862ed81d36fea42344c6f3
varun9110 <varun.goel.vg@gmail.com>
2023-05-02 19:54:42 +0530

    feat: Problem 1389

---
+++ 1389_Create_Target_Array_in_the_Given_Order.js
@@ -0,0 +1,52 @@
+/**
+ * 1389. Create Target Array in the Given Order
+ * Difficulty: Easy
+ * Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+It is guaranteed that the insertion operations will be valid.
+Example 1:
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+Example 2:
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+Example 3:
+Input: nums = [1], index = [0]
+Output: [1]
+
+Constraints:
+1 <= nums.length, index.length <= 100
+nums.length == index.length
+0 <= nums[i] <= 100
+0 <= index[i] <= i
+*/
+
+/**
+ * Approach:
+ * create a blank result array, then create a loop to iterate through all the numbers in the array and keep slicing them in the result array.
+ */
+
+var createTargetArray = function (nums, index) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    result.splice(index[i], 0, nums[i]);
+  }
+  return result;
+};
