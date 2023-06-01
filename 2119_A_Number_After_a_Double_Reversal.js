acb9a11d166aa69d062b769f92c59be397b51b47
varun9110 <varun.goel.vg@gmail.com>
2023-06-01 21:31:00 +0530

    feat: Problem 2119

---
+++ 2119_A_Number_After_a_Double_Reversal.js
@@ -0,0 +1,43 @@
+/**
+ * 2119. A Number After a Double Reversal
+ * Difficulty: easy
+ * Reversing an integer means to reverse all its digits.
+For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+Example 1:
+Input: num = 526
+Output: true
+Explanation: Reverse num to get 625, then reverse 625 to get 526, which equals num.
+Example 2:
+Input: num = 1800
+Output: false
+Explanation: Reverse num to get 81, then reverse 81 to get 18, which does not equal num.
+Example 3:
+Input: num = 0
+Output: true
+Explanation: Reverse num to get 0, then reverse 0 to get 0, which equals num.
+
+Constraints:
+0 <= num <= 106
+ */
+
+/**
+ * Approach: 
+ * Basically we are checking if the number is 0 then return true
+ * if it is divisible by 10 then definitely not possible thus return false
+ * else return true for rest of the scenarios
+ */
+
+var isSameAfterReversals = function(num) {
+  if(num == 0) return true;
+  if(num%10 === 0) return false;
+  return true;
+};
+
+/**
+ * one liner for this
+ */
+
+var isSameAfterReversals = function(num) {
+    return !num || num%10;
+};
