817ba89a1d30f0f13cd6ba3f5213fe5b7916e805
varun9110 <varun.goel.vg@gmail.com>
2023-06-06 22:04:55 +0530

    feat: Problem 1071

---
+++ 1071_Greatest_Common_Divisor_of_Strings.js
@@ -0,0 +1,37 @@
+/**
+ * 1071. Greatest Common Divisor of Strings
+ * For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+Example 1:
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+Constraints:
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+ */
+
+const gcdOfStrings = (str1, str2) => {
+  if (str1 + str2 !== str2 + str1) return '';
+  const gcd = (a, b) => (0 === b ? a : gcd(b, a % b));
+  return str1.substring(0, gcd(str1.length, str2.length));
+};
+
+
+
+var gcdOfStrings = function(str1, str2) {
+    const [smallerString, largerString] = [str1, str2].sort((a,b) => a.length - b.length);
+    for (let i = smallerString.length; i > 0; i--) {
+        const testString = smallerString.slice(0, i);
+        const correctSmaller = !smallerString.split(testString).join('').length;
+        const correctLarger = !largerString.split(testString).join('').length;
+        if (correctSmaller && correctLarger) return testString;
+    }
+    return '';
+};
