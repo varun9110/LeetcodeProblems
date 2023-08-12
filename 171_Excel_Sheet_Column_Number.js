42079e57b8bbef0ec7f3900f101261450d935fee
varun9110 <varun.goel.vg@gmail.com>
2023-08-12 10:45:20 +0530

    feat: Problem 171

---
+++ 171_Excel_Sheet_Column_Number.js
@@ -0,0 +1,43 @@
+/**
+ * 171. Excel Sheet Column Number
+ * Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+
+Example 1:
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+Input: columnTitle = "ZY"
+Output: 701
+
+Constraints:
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+ */
+
+var titleToNumber = function(columnTitle) {
+    const charCodeBase = 'A'.charCodeAt(0) - 1;
+    const n = columnTitle.length;
+    let number = 0;
+
+    /* 
+     * Think of it as base 26. For example,
+     * Column number of "AB" = 1 * 26^1 + 2 * 26^0 
+     */
+    for (let i = 0; i < n; i++)
+        number += (columnTitle.charCodeAt(i) - charCodeBase) * Math.pow(26, n-i-1);
+    
+    return number;
+};
