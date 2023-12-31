29579e6a082f16b2c439ec08746efa267114d70f
varun9110 <varun.goel.vg@gmail.com>
2023-12-31 18:10:53 +0530

    feat: problem 345

---
+++ 345_Reverse_Vowels_of_a_String.js
@@ -0,0 +1,34 @@
+/**
+ * 345. Reverse Vowels of a String
+ * Difficulty: Easy
+ * 
+ * Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+Example 1:
+Input: s = "hello"
+Output: "holle"
+Example 2:
+Input: s = "leetcode"
+Output: "leotcede"
+Constraints:
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters.
+ */
+
+var reverseVowels = function(s) {
+     const VOWELS = { 'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1 };
+    const arr = s.split('');
+    let i = 0, j = arr.length - 1;
+    while (i < j) {
+        if (VOWELS[arr[i]] && VOWELS[arr[j]]) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            i++;
+            j--;
+        } else if (VOWELS[arr[i]]) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+    return arr.join('');
+};
