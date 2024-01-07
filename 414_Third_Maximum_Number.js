210665753b67bbf0f3022ea3836b1b122418aa62
varun9110 <varun.goel.vg@gmail.com>
2024-01-07 20:28:10 +0530

    feat: problem 414

---
+++ 414_Third_Maximum_Number.js
@@ -0,0 +1,38 @@
+/**
+ * 414. Third Maximum Number
+ * Difficulty: Easy
+ * 
+ * Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+Example 1:
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+Example 2:
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+Constraints:
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ */
+
+var thirdMax = function(nums) {
+  var uniq = [...new Set(nums)];
+    if(uniq.length === 2 ) return Math.max(uniq[1],uniq[0])
+    if(uniq.length === 1 ) return uniq[0]
+
+    return uniq.sort((a,b)=>b-a)[2]
+};
