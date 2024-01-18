38c625f29e7625e1b7f2ffddb891dd4e02706900
varun9110 <varun.goel.vg@gmail.com>
2024-01-18 12:11:44 +0530

    chore: problem 70 update

--- 70_Climbing_Stairs.js
+++ 70_Climbing_Stairs.js
@@ -32,3 +32,23 @@ Constraints:
      return climbStairs(n - 2) + climbStairs(n - 1);
  };
  
+
+ /**
+  * using the for loop
+  */
+
+ var climbStairs = function(n) {
+
+    return memoFunc(n, {});
+    
+};
+
+const memoFunc = (n, memo) => {
+    if(n in memo) return memo[n];
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+
+    memo[n] = memoFunc(n - 1, memo) + memoFunc(n - 2, memo);
+    return memo[n];
+};
