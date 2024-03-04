064ea3823deabfa2f2c587d0d3da47b84b423869
Varun Goel <varun.goel.vg@gmail.com>
2024-03-03 20:30:00 -0600

    feat: problem 19

--- 19_Remove_Nth_Node_From_End_of_List.js
+++ 19_Remove_Nth_Node_From_End_of_List.js
@@ -52,7 +52,7 @@ Otherwise, once we succesfully find the node before the target, we can then stit
 
 var removeNthFromEnd = function(head, n) {
     let fast = head, slow = head
-    for (let i = 0; i < n; i++) fast = fast.next
+    for (let i = 0; i < n; i++) fast = fast.next 
     if (!fast) return head.next
     while (fast.next) fast = fast.next, slow = slow.next
     slow.next = slow.next.next
