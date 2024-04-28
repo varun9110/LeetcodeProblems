4c10f59a660d858f0db8ddc899731a22d0744f17
varun9110 <varun.goel.vg@gmail.com>
2024-04-28 19:33:25 +0530

    feat: problem 834

---
+++ 834_Sum_of_Distances_in_Tree.js
@@ -0,0 +1,75 @@
+/**
+ * 834. Sum of Distances in Tree
+ * Difficulty: Hard
+ * 
+ * There is an undirected connected tree with n nodes labeled from 0 to n - 1 and n - 1 edges.
+You are given the integer n and the array edges where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
+Return an array answer of length n where answer[i] is the sum of the distances between the ith node in the tree and all other nodes.
+
+
+Example 1:
+Input: n = 6, edges = [[0,1],[0,2],[2,3],[2,4],[2,5]]
+Output: [8,12,6,10,10,10]
+Explanation: The tree is shown above.
+We can see that dist(0,1) + dist(0,2) + dist(0,3) + dist(0,4) + dist(0,5)
+equals 1 + 1 + 2 + 2 + 2 = 8.
+Hence, answer[0] = 8, and so on.
+Example 2:
+Input: n = 1, edges = []
+Output: [0]
+Example 3:
+
+Input: n = 2, edges = [[1,0]]
+Output: [1,1]
+ 
+Constraints:
+1 <= n <= 3 * 104
+edges.length == n - 1
+edges[i].length == 2
+0 <= ai, bi < n
+ai != bi
+The given input represents a valid tree.
+ */
+
+/**
+ * Approach 📝
+Construct the graph and count the number of nodes in each subtree using a depth-first search (dfs1).
+Calculate the sum of distances for each node using the subtree sizes calculated in step 1 (dfs1).
+Traverse the tree again using another depth-first search (dfs2) to update the sum of distances for each node considering its parent node.
+ */
+
+var sumOfDistancesInTree = function(n, edges) {
+    const graph = new Array(n).fill(null).map(() => []);
+    const count = new Array(n).fill(0);
+    const res = new Array(n).fill(0);
+    
+    for (const [u, v] of edges) {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    
+    const dfs1 = (cur, parent) => {
+        count[cur] = 1;
+        for (const child of graph[cur]) {
+            if (child !== parent) {
+                dfs1(child, cur);
+                count[cur] += count[child];
+                res[cur] += res[child] + count[child];
+            }
+        }
+    };
+    
+    const dfs2 = (cur, parent) => {
+        for (const child of graph[cur]) {
+            if (child !== parent) {
+                res[child] = res[cur] + n - 2 * count[child];
+                dfs2(child, cur);
+            }
+        }
+    };
+    
+    dfs1(0, -1);
+    dfs2(0, -1);
+    
+    return res;
+};
