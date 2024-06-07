9d188171ecd9ba1da3060249410a92dc3b7b83a9
varun9110 <varun.goel.vg@gmail.com>
2024-06-07 20:26:53 +0530

    feat: problem 648

---
+++ 648_Replace_Words.js
@@ -0,0 +1,50 @@
+/**
+ * 648. Replace Words
+ * Difficulty: Medium
+ * 
+ * In English, we have a concept called root, which can be followed by some other word to form another longer word - 
+ * let's call this word derivative. For example, when the root "help" is followed by the word "ful", we can form a derivative "helpful".
+
+Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the derivatives 
+in the sentence with the root forming it. If a derivative can be replaced by more than one root, replace it with the root that has the shortest length.
+
+Return the sentence after the replacement.
+
+Example 1:
+Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+Example 2:
+Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+Output: "a a b c"
+
+Constraints:
+
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 100
+dictionary[i] consists of only lower-case letters.
+1 <= sentence.length <= 106
+sentence consists of only lower-case letters and spaces.
+The number of words in sentence is in the range [1, 1000]
+The length of each word in sentence is in the range [1, 1000]
+Every two consecutive words in sentence will be separated by exactly one space.
+sentence does not have leading or trailing spaces.
+ */
+
+/**
+ * Approach: O(n^2) approach
+ */
+
+var replaceWords = function(dictionary, sentence) {
+    dictionary.sort((a,b) => a.length - b.length);
+    let sen = sentence.split(" ");
+
+    for(let i=0; i<dictionary.length; i++){
+        let dicLen = dictionary[i].length
+        for(let j=0; j<sen.length; j++){
+            if(dicLen <= sen[j].length && (dictionary[i] === sen[j].substring(0, dicLen))) {
+                sen[j] = dictionary[i]
+            }
+        }
+    }
+    return sen.join(" ")
+};
