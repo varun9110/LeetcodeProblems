fd2c01b6ca47712fe7892ffac4fb77adbc743d32
varun9110 <varun.goel.vg@gmail.com>
2024-08-06 20:56:28 +0530

    feat: problem 3016

---
+++ 3016_Minimum_Number_of_Pushes_to_Type_Word_II.js
@@ -0,0 +1,91 @@
+/**
+ * 3016. Minimum Number of Pushes to Type Word II
+ * Difficulty: Medium
+ * 
+ * You are given a string word containing lowercase English letters.
+
+Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to form words by pushing them. For example, 
+the key 2 is mapped with ["a","b","c"], we need to push the key one time to type "a", two times to type "b", and three times to type "c" .
+It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to any amount of letters, 
+but each letter must be mapped to exactly one key. You need to find the minimum number of times the keys will be pushed to type the string word.
+Return the minimum number of pushes needed to type word after remapping the keys.
+An example mapping of letters to keys on a telephone keypad is given below. Note that 1, *, #, and 0 do not map to any letters.
+
+
+Example 1:
+Input: word = "abcde"
+Output: 5
+Explanation: The remapped keypad given in the image provides the minimum cost.
+"a" -> one push on key 2
+"b" -> one push on key 3
+"c" -> one push on key 4
+"d" -> one push on key 5
+"e" -> one push on key 6
+Total cost is 1 + 1 + 1 + 1 + 1 = 5.
+It can be shown that no other mapping can provide a lower cost.
+Example 2:
+
+Input: word = "xyzxyzxyzxyz"
+Output: 12
+Explanation: The remapped keypad given in the image provides the minimum cost.
+"x" -> one push on key 2
+"y" -> one push on key 3
+"z" -> one push on key 4
+Total cost is 1 * 4 + 1 * 4 + 1 * 4 = 12
+It can be shown that no other mapping can provide a lower cost.
+Note that the key 9 is not mapped to any letter: it is not necessary to map letters to every key, but to map all the letters.
+Example 3:
+
+Input: word = "aabbccddeeffgghhiiiiii"
+Output: 24
+Explanation: The remapped keypad given in the image provides the minimum cost.
+"a" -> one push on key 2
+"b" -> one push on key 3
+"c" -> one push on key 4
+"d" -> one push on key 5
+"e" -> one push on key 6
+"f" -> one push on key 7
+"g" -> one push on key 8
+"h" -> two pushes on key 9
+"i" -> one push on key 9
+Total cost is 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 1 * 2 + 2 * 2 + 6 * 1 = 24.
+It can be shown that no other mapping can provide a lower cost.
+ 
+Constraints:
+1 <= word.length <= 105
+word consists of lowercase English letters.
+ */
+
+/**
+ * Approach: find the letter with highest frequency, that needs to have 1 click, this create a mapper with all the frequency of each letter
+ * then since we have 7 keys then create a count, if the  count / 8, and find the multiplier. this multiplier is used to check the number of times
+ * we need to click the button.
+ * now just iterate through the object of mapper and check all the clicks that we need to make
+ */
+
+var minimumPushes = function(word) {
+    let mapper = {};
+
+    for(let i=0; i<word.length; i++){
+        mapper[word[i]] = (mapper[word[i]]) ? mapper[word[i]]+1 : 1
+    }
+    let clickCount = 0;
+    let count = 0;
+    while(Object.entries(mapper).length>0){
+        console.log(Object.entries(mapper))
+        let maxVal = 0;
+        let maxKey = 0;
+        for (let [key, val] of Object.entries(mapper)){
+            if(val > maxVal){
+                maxVal = val;
+                maxKey = key
+            }
+        }
+        let multiplier = Math.floor(count/8);
+        clickCount += (multiplier+1) * (maxVal)
+        count++;
+        delete mapper[maxKey]
+    }
+    return clickCount
+
+};
