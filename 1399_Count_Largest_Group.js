4010e4657ed0269b5f1d1cca0d8e7f7d672e9465
Varun Goel <varun.goel.vg@gmail.com>
2025-04-22 21:25:14 -0400

    feat: problem 401

--- 1399_Count_Largest_Group.js
+++ 1399_Count_Largest_Group.js
@@ -22,22 +22,29 @@ Constraints:
  */
 
 
-var getSum = function(n) {
-    let sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n = Math.floor(n / 10);
-    }
-    return sum;
-}
-
+/**
+ * @param {number} n
+ * @return {number}
+ */
 var countLargestGroup = function(n) {
-    let m = new Map(), max = 0;
+    const groups = new Map();
+
     for (let i = 1; i <= n; i++) {
-        let sum = getSum(i);
-        m.set(sum, m.get(sum)+1 || 1);
-        max = Math.max(max, m.get(sum));
-        console.log(m);
+        let sum = 0, num = i;
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        groups.set(sum, (groups.get(sum) || 0) + 1);
     }
-    return Array.from(m.values()).filter(num => num === max).length;
+
+    const maxSize = Math.max(...groups.values());
+
+    let res = 0;
+    for (let count of groups.values()) {
+        if (count === maxSize) res++;
+    }
+
+    return res;
 };
+
