63fe9ca84abe252c102febd30e4925e97b35f33a
Varun Goel <varun.goel.vg@gmail.com>
2025-05-16 22:28:21 -0400

    feat: problem 415

---
+++ 415_Add_Strings.js
@@ -0,0 +1,46 @@
+/**
+ * 415. Add Strings
+ * Difficulty: Easy
+ * 
+ * Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+Example 1:
+Input: num1 = "11", num2 = "123"
+Output: "134"
+Example 2:
+Input: num1 = "456", num2 = "77"
+Output: "533"
+Example 3:
+Input: num1 = "0", num2 = "0"
+Output: "0"
+
+Constraints:
+1 <= num1.length, num2.length <= 104
+num1 and num2 consist of only digits.
+num1 and num2 don't have any leading zeros except for the zero itself.
+ */
+
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1, j = num2.length - 1;
+    let carry = 0, res = '';
+
+    while (i >= 0 || j >= 0 || carry) {
+        const n1 = i >= 0 ? +num1[i] : 0;
+        const n2 = j >= 0 ? +num2[j] : 0;
+        const sum = n1 + n2 + carry;
+        res = (sum % 10) + res;
+        carry = Math.floor(sum / 10);
+        i--;
+        j--;
+    }
+
+    return res;
+};
