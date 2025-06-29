ba28b4c5b060adf27fa0bd209f09ae909c8b3a13
Varun Goel <varun.goel.vg@gmail.com>
2025-06-28 22:52:11 -0400

    feat: problem 1498

---
+++ 1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.js
@@ -0,0 +1,105 @@
+/**
+ * 1498. Number of Subsequences That Satisfy the Given Sum Condition
+ * Difficulty: Medium
+ * 
+ * You are given an array of integers nums and an integer target.
+
+Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: nums = [3,5,6,7], target = 9
+Output: 4
+Explanation: There are 4 subsequences that satisfy the condition.
+[3] -> Min value + max value <= target (3 + 3 <= 9)
+[3,5] -> (3 + 5 <= 9)
+[3,5,6] -> (3 + 6 <= 9)
+[3,6] -> (3 + 6 <= 9)
+Example 2:
+
+Input: nums = [3,3,6,8], target = 10
+Output: 6
+Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
+[3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+Example 3:
+
+Input: nums = [2,3,3,4,6,7], target = 12
+Output: 61
+Explanation: There are 63 non-empty subsequences, two of them do not satisfy the condition ([6,7], [7]).
+Number of valid subsequences (63 - 2 = 61).
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+1 <= target <= 106
+ */
+
+/**
+ * You are given an array of integers nums and an integer target.
+
+Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: nums = [3,5,6,7], target = 9
+Output: 4
+Explanation: There are 4 subsequences that satisfy the condition.
+[3] -> Min value + max value <= target (3 + 3 <= 9)
+[3,5] -> (3 + 5 <= 9)
+[3,5,6] -> (3 + 6 <= 9)
+[3,6] -> (3 + 6 <= 9)
+Example 2:
+
+Input: nums = [3,3,6,8], target = 10
+Output: 6
+Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
+[3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+Example 3:
+
+Input: nums = [2,3,3,4,6,7], target = 12
+Output: 61
+Explanation: There are 63 non-empty subsequences, two of them do not satisfy the condition ([6,7], [7]).
+Number of valid subsequences (63 - 2 = 61).
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+1 <= target <= 106
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var numSubseq = function(nums, target) {
+    const mod = 1e9 + 7;
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+
+    const power = new Array(n).fill(1);
+    for (let i = 1; i < n; i++) {
+        power[i] = (power[i - 1] * 2) % mod;
+    }
+
+    let left = 0, right = n - 1, result = 0;
+
+    while (left <= right) {
+        if (nums[left] + nums[right] <= target) {
+            result = (result + power[right - left]) % mod;
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return result;
+};
