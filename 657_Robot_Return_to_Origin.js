66ddd50865494788c0e68c16e1592d820c4e741e
Varun Goel <varun.goel.vg@gmail.com>
2025-07-13 23:32:31 -0400

    feat: problem 657

--- 657_Robot_Return_to_Origin.js
+++ 657_Robot_Return_to_Origin.js
@@ -30,11 +30,17 @@ Constraints:
 moves only contains the characters 'U', 'D', 'L' and 'R'.
  */
 
-let l = 0, r = 0, u = 0, d = 0;
-for (let move of moves) {
-    if (move === 'L') l++;
-    else if (move === 'R') r++;
-    else if (move === 'U') u++;
-    else if (move === 'D') d++;
-}
-return l === r && u === d;
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let l = 0, r = 0, u = 0, d = 0;
+    for (let move of moves) {
+        if (move === 'L') l++;
+        else if (move === 'R') r++;
+        else if (move === 'U') u++;
+        else if (move === 'D') d++;
+    }
+    return l === r && u === d;
+};
