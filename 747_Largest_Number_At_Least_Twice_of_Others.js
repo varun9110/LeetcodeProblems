c53009c46c71eb546632fdbe7d0a00be88843b83
Varun Goel <varun.goel.vg@gmail.com>
2025-07-20 22:31:23 -0400

    feat: problem 747

---
+++ 747_Largest_Number_At_Least_Twice_of_Others.js
@@ -0,0 +1,48 @@
+/**
+ * 747. Largest Number At Least Twice of Others
+ * Difficulty: Easy
+ * 
+ * You are given an integer array nums where the largest integer is unique.
+
+Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [3,6,1,0]
+Output: 1
+Explanation: 6 is the largest integer.
+For every other number in the array x, 6 is at least twice as big as x.
+The index of value 6 is 1, so we return 1.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: -1
+Explanation: 4 is less than twice the value of 3, so we return -1.
+ 
+
+Constraints:
+
+2 <= nums.length <= 50
+0 <= nums[i] <= 100
+The largest element in nums is unique.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+    let max = -1, second = -1, index = -1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) {
+            second = max;
+            max = nums[i];
+            index = i;
+        } else if (nums[i] > second) {
+            second = nums[i];
+        }
+    }
+    return max >= 2 * second ? index : -1;
+};
