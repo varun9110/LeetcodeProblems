54991bfe31482d42dbec1dc84c6e604f9e8e3630
Varun Goel <varun.goel.vg@gmail.com>
2025-09-30 23:07:41 -0400

    feat: problem 897

---
+++ 897_Increasing_Order_Search_Tree.js
@@ -0,0 +1,54 @@
+/**
+ * 897. Increasing Order Search Tree
+ * Difficulty: easy
+ * 
+ * Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+ 
+
+Constraints:
+
+The number of nodes in the given tree will be in the range [1, 100].
+0 <= Node.val <= 1000
+ */
+
+//JavaScript Code
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+    var ptr = new TreeNode(-1)
+    var curr = ptr
+    inorder = ((root) => {
+        if(root == undefined)
+            return
+        inorder(root.left)
+        curr.right = new TreeNode(root.val)
+        curr = curr.right
+        inorder(root.right)
+        return
+    })
+    inorder(root)
+    return ptr.right
+};
