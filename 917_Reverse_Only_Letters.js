8cf910ce59363ca8db86ad532ca7128d498bdd71
varun9110 <varun.goel.vg@gmail.com>
2025-10-25 12:16:16 -0400

    feat: problem 917

---
+++ 917_Reverse_Only_Letters.js
@@ -0,0 +1,57 @@
+/**
+ * 917. Reverse Only Letters
+ * Difficulty: Easy
+ * 
+ * Given a string s, reverse the string according to the following rules:
+
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+
+ 
+
+Example 1:
+
+Input: s = "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of characters with ASCII values in the range [33, 122].
+s does not contain '\"' or '\\'.
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseOnlyLetters = function(s) {
+    let letters = [];
+  for (let i = 0; i < s.length; i++) {
+    if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+      letters.push(s[i]);
+    }
+  }
+  letters = letters.reverse();
+  let newStr = "";
+  let k = 0;
+  for (let i = 0; i < s.length; i++) {
+    if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+      newStr += letters[k];
+      k++;
+    } else {
+      newStr += s[i];
+    }
+  }
+  return newStr;
+};
