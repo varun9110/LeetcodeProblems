71b0c0de00f49c41c153ec064e7a0887f77d1951
Varun Goel <varun.goel.vg@gmail.com>
2025-11-26 22:28:32 -0500

    feat: problem 3381

---
+++ 3381_Maximum_Subarray_Sum_With_Length_Divisible_by_K.js
@@ -0,0 +1,112 @@
+/**
+ * 3381. Maximum Subarray Sum With Length Divisible by K
+ * Difficulty: Medium
+ * 
+ * You are given an array of integers nums and an integer k.
+
+Return the maximum sum of a subarray of nums, such that the size of the subarray is divisible by k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2], k = 1
+
+Output: 3
+
+Explanation:
+
+The subarray [1, 2] with sum 3 has length equal to 2 which is divisible by 1.
+
+Example 2:
+
+Input: nums = [-1,-2,-3,-4,-5], k = 4
+
+Output: -10
+
+Explanation:
+
+The maximum sum subarray is [-1, -2, -3, -4] which has length equal to 4 which is divisible by 4.
+
+Example 3:
+
+Input: nums = [-5,1,2,-3,4], k = 2
+
+Output: 4
+
+Explanation:
+
+The maximum sum subarray is [1, 2, -3, 4] which has length equal to 4 which is divisible by 2.
+
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 2 * 105
+-109 <= nums[i] <= 109
+ */
+
+/**
+ * Prerequisite
+First, let's recall the core Algorithm used in this problem:
+
+Kadane’s Algorithm: Finds the maximum subarray sum (in linear time):
+
+image.png
+
+subMax→ candidate maximum subarray ending at index i
+
+Here, the max subarray is 6, which corresponds to subarray:
+
+[4,−1,2,1]
+
+image.png
+
+Read the proof here:
+Maximum Subarray Problem
+
+General Case:
+This is almost the same thing, but now, we need to determine the corresponding length of each (subMax):
+
+image.png
+
+To compute the length of subMax:
+Just track the index of the minSoFar (idxMin), and subtract to the current index (idxSub).
+
+Note that idxSub is simply the global index. (relabeled for clarity).
+
+image.png
+
+Valid Subarrays
+Finally, determine the valid subMax with lengths length%k==0, and return the global maximum.
+
+image.png
+
+image.png
+
+Optimization:
+After learning the core algorithm.
+
+Now, we can solve this using only O(k) extra space.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarraySum = function(nums, k) {
+     let prefSum = 0;
+    let subMax = -Infinity;
+
+    const minSoFar = new Array(k).fill(Infinity);
+    minSoFar[(k - 1) % k] = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        prefSum += nums[i];
+        subMax = Math.max(subMax, prefSum - minSoFar[i % k]);
+        minSoFar[i % k] = Math.min(minSoFar[i % k], prefSum);
+    }
+
+    return subMax;
+};
