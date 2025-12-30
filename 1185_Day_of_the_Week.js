2ca067758d75ada8306bdcf14c58e4d83061f7ee
varun9110 <varun.goel.vg@gmail.com>
2025-12-29 21:54:21 -0500

    feat: problem 1185

---
+++ 1185_Day_of_the_Week.js
@@ -0,0 +1,47 @@
+/**
+ * 1185. Day of the Week
+ * Difficulty: Easy
+ * 
+ * Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+
+Note: January 1, 1971 was a Friday.
+
+ 
+
+Example 1:
+
+Input: day = 31, month = 8, year = 2019
+Output: "Saturday"
+Example 2:
+
+Input: day = 18, month = 7, year = 1999
+Output: "Sunday"
+Example 3:
+
+Input: day = 15, month = 8, year = 1993
+Output: "Sunday"
+ 
+
+Constraints:
+
+The given dates are valid dates between the years 1971 and 2100.
+ */
+
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+    
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+
+    const newDate = new Date(year, month - 1, day); // month is 0-based 
+    
+    return days[newDate.getDay()];
+};
