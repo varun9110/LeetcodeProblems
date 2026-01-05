217f60652d54243bd617d504a3d3769d840e3c19
varun9110 <varun.goel.vg@gmail.com>
2026-01-04 22:26:45 -0500

    feat: problem 1232

---
+++ 1232_Check_If_It_Is_a_Straight_Line.js
@@ -0,0 +1,54 @@
+/**
+ * 1232. Check If It Is a Straight Line
+ * Difficulty: Easy
+ * 
+ * You are given an integer array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+
+Example 1:
+
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+Example 2:
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+
+Constraints:
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+ */
+
+
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+// For three points to be collinear, the slope between points 1-2 should equal the slope between points 2-3:
+// (y2 - y1) / (x2 - x1) = (y3 - y2) / (x3 - x2)
+
+var checkStraightLine = function(coordinates) {
+    const n = coordinates.length;
+    
+    // base case : if there're only 2 points, return true.
+    if(n <= 2){
+        return true;
+    }
+    
+    // step 1 - check every consecutive triplet of points
+    for(let i=0; i<n-2; i++){
+        // extract co-ordinates
+        const [x1, y1] = coordinates[i];
+        const [x2, y2] = coordinates[i+1];
+        const [x3, y3] = coordinates[i+2];
+
+        // step 2 - cross multiplication to check for collinearity
+        if((y2 - y1) * (x3 - x2) !== (y3 - y2) * (x2 - x1)){
+            return false;
+        } 
+    }
+
+    // step 3 - return true as all points are collinear that forms straight line
+    return true;
+};
