a2cf5fe301a2b27dd46ae6e472fff1306a6b0267
varun9110 <varun.goel.vg@gmail.com>
2026-02-20 22:33:17 -0500

    feat: problem 762

--- 762_Prime_Number_of_Set_Bits_in_Binary_Representation.js
+++ 762_Prime_Number_of_Set_Bits_in_Binary_Representation.js
@@ -56,3 +56,35 @@ var countPrimeSetBits = function(left, right) {
 
     return result;
 };
+
+
+/**
+ * Refined approach
+ */
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var countPrimeSetBits = function(left, right) {
+    let ans = 0
+    for(let n = left;n<=right;n++){
+        let c = 0
+        let a = n
+        while(a>0){
+            c += a&1
+            a = a>>1
+        }
+        let p = true
+        for(let i=2;i<=c/2;i++){
+            if(c%i===0){
+                p = false
+                break
+            }
+        }
+        // console.log(n,c,p)
+        if(p&& c!==1)ans++
+    }
+    return ans
+};
