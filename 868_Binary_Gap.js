caa5b4104e062c0987c93a887f75280dbf3bae8b
varun9110 <varun.goel.vg@gmail.com>
2026-02-21 23:00:36 -0500

    feat: problem 868

--- 868_Binary_Gap.js
+++ 868_Binary_Gap.js
@@ -51,3 +51,28 @@ var binaryGap = function (n) {
   }
   return max;
 };
+
+
+/**
+ * faster approach
+ */
+
+var binaryGap = function (n) {
+  n = n.toString(2);
+
+  let currentСount = 0;
+  let maxCount = 0;
+
+  for (let i = 1; i < n.length; i++) {
+    if (maxCount <= currentСount && n[i] === "1") {
+      currentСount++;
+      maxCount = currentСount;
+    }
+    if (n[i] !== "1") {
+      currentСount++;
+    } else {
+      currentСount = 0;
+    }
+  }
+  return maxCount;
+};
